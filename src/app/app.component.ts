24403d164d8f93f3ff001350b9f66e160ce115b5
B BhanuBalaji <bezawada_balaji@nextazy.com>
2024-08-23 18:58:03 +0530

    add custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angularpro';
+export class AppComponent implements OnInit {
+  title = 'angular-project-MF2';
+  DataFromApp1:any=''
+constructor(
+  private cdr:ChangeDetectorRef
+) {}
+
+
+  ngOnInit(): void {
+    // In Angular application 2
+    window.addEventListener('app1Event', (event:any) => {
+      console.log('Received event from App 1:', event?.detail);
+      this.DataFromApp1=event?.detail?.message
+      console.log(this.DataFromApp1)
+      this.cdr.detectChanges();
+      // Handle the event or update the UI
+    });
+    
+      }
 }
