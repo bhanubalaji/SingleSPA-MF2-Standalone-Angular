77dc5e3ac88607f69063ae72501a2595b6f3e6e8
B BhanuBalaji <bezawada_balaji@nextazy.com>
2024-08-29 09:31:27 +0530

    add auth guard

---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard  {
+
+  constructor(private router:Router){}
+  canActivate(): boolean {
+    if (!localStorage.getItem('token')) {
+      this.router.navigate(['auth/login']);
+      return false;
+    }
+    return true;
+  }
+}
