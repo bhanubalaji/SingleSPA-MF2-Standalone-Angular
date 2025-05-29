ea3156257db2f073601a21f102c0e7ad95e89e25
B BhanuBalaji <bezawada_balaji@nextazy.com>
2025-05-29 11:17:32 +0530

    Revert "add console log"
    This reverts commit bf2d616552352cee10a2ba005704907d50fdf647.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,29 +23,26 @@ export class AppComponent implements OnInit {
 
   Datacustombus: any
   ngOnInit(): void {
-      console.log('state initializedMF2');
+    //click Custom Events
+    window.addEventListener('app1Event', (event: any) => {
+      console.log('Received event from App 1:', event?.detail);
+      this.DataFromApp1 = event?.detail?.message
+      console.log(this.DataFromApp1)
+      this.cdr.detectChanges();
 
-      //click Custom Events
-      window.addEventListener('app1Event', (event: any) => {
-        console.log('Received event from App 1:', event?.detail);
-        this.DataFromApp1 = event?.detail?.message
-        console.log(this.DataFromApp1)
-        this.cdr.detectChanges();
-  
-        // custom bus
-        window.addEventListener('message', (event) => {
-          if (event.origin !== 'http://localhost:3000') {
-            return; // Ignore messages from unexpected origins
-          }
-          const message = event.data?.message;
-          if (message) {
-            console.log('Received message---------------:', message);
-            this.Datacustombus = message
-            this.cdr.detectChanges();
-          }
-        });
+      // custom bus
+      window.addEventListener('message', (event) => {
+        if (event.origin !== 'http://localhost:9000') {
+          return; // Ignore messages from unexpected origins
+        }
+        const message = event.data?.message;
+        if (message) {
+          console.log('Received message---------------:', message);
+          this.Datacustombus = message
+          this.cdr.detectChanges();
+        }
       });
- 
+    });
 
 
 
